fdaa00517b8867ed31d4ba21e43b4e25333cbe85
Tulugu Jayanth <87133065+18jayanth@users.noreply.github.com>
2024-08-08 13:53:52 +0530

    Time: 3 ms (70.33%) | Memory: 11.2 MB (54.99%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -6,7 +6,7 @@ public:
         sort(strs.begin(),strs.end());
         string left=strs[0];
         string right=strs[n-1];
-        for(int i=0;i<min(left.size(),right.size());i++)
+        for(int i=0;i<left.size();i++)
         {
             if(left[i]==right[i])
             {
